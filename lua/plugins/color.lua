322015907c30db3300af852e16936e0de647f9df
Namsom007 <deawffe0@gmail.com>
2025-12-02 01:15:17 +0700

    change color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -6,22 +6,23 @@ end
 return {
     {
         "sainnhe/gruvbox-material",
+        priority = 1000,
         config = function()
             vim.g.gruvbox_material_background = "medium"
             vim.cmd.colorscheme("gruvbox-material")
-            enable_transparency()
-	    -- ปรับสี Tree-sitter group บางตัว
-	    vim.api.nvim_set_hl(0, "TSFunction", { fg = "#FFD700", bold = true }) -- ฟังก์ชันเป็นสีทอง
-	    vim.api.nvim_set_hl(0, "TSKeyword",  { fg = "#FF5733", italic = true }) -- keyword เป็นสีส้ม
-	    vim.api.nvim_set_hl(0, "TSString",   { fg = "#98C379" }) -- string เป็นสีเขียวสด
-	    vim.api.nvim_set_hl(0, "TSComment",  { fg = "#808080", italic = true }) -- คอมเมนต์เป็นเทา
 
+            enable_transparency()
+
+            vim.api.nvim_set_hl(0, "@function", { fg = "#FFD700", bold = true })
+            vim.api.nvim_set_hl(0, "@keyword",  { fg = "#FF5733", italic = true })
+            vim.api.nvim_set_hl(0, "@string",   { fg = "#98C379" })
+            vim.api.nvim_set_hl(0, "@comment",  { fg = "#808080", italic = true })
         end
     },
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = { theme = 'gruvbox-material' }
+        opts = { theme = "gruvbox-material" }
     },
 }
 
